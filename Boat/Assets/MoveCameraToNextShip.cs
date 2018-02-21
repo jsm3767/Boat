d67c07732b8834279afc25b969e83d92d84af97a
Benjamin Morgan (RIT Student) <bxm8563@ad.rit.edu>
2018-02-21 13:49:14 -0500

    Added a button to go through your ships with smooth transitions

--- Boat/Assets/MoveCameraToNextShip.cs
+++ Boat/Assets/MoveCameraToNextShip.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Get currentShip from gameManager
 public class MoveCameraToNextShip : MonoBehaviour
 {
     private GameObject[] playerShips;
-    private GameObject currentShip;
-    private float xOffset = -2.3f;
+    private GameObject currentShip = null;
+    private float zOffset = -2.3f;
 
     // Use this for initialization
     void Start()
@@ -21,19 +22,37 @@ public class MoveCameraToNextShip : MonoBehaviour
         int currentIndex = 0;
         if (currentShip != null)
         {
-            for( int index = 0; index < playerShips.Length; index++ )
+            currentShip.GetComponentInChildren<Canvas>().enabled = false;
+
+            for ( int index = 0; index < playerShips.Length; index++ )
             {
                 if (playerShips[index].GetInstanceID() == currentShip.GetInstanceID())
                 {
                     currentIndex = index;
                 }
             }
-
+            
             currentIndex = (currentIndex + 1) % playerShips.Length;
         }
 
+        currentShip = playerShips[currentIndex];
         playerShips[currentIndex].GetComponentInChildren<Canvas>().enabled = true;
-        Camera.main.transform.position = new Vector3(playerShips[currentIndex].transform.position.x + xOffset, 5.0f, playerShips[currentIndex].transform.position.z);
+
+        //start coroutine move camera
+        StartCoroutine(SmoothCameraMove(.2f, new Vector3(playerShips[currentIndex].transform.position.x, 5.0f, playerShips[currentIndex].transform.position.z + zOffset)));
+        
     }
 
+
+    IEnumerator SmoothCameraMove(float transitionDuration, Vector3 target)
+    {
+        Vector3 startLocation = Camera.main.transform.position;
+        float t = 0.0f;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * (Time.timeScale / transitionDuration);
+            Camera.main.transform.position = Vector3.Lerp(startLocation, target, t);
+            yield return 0;
+        }
+    }
 }
