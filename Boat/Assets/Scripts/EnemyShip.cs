21c1b5015a1bceb69fdd21d8f09cf727dff1bfd2
Aria Myers <jsm3767@g.rit.edu>
2018-03-07 10:49:19 -0500

    Beginning enemy script and scene adjustments

---
+++ Boat/Assets/Scripts/EnemyShip.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShip : Ship {
+
+	private List<GameObject> playerShipsOBJ = new List<GameObject>();
+
+
+	// Use this for initialization
+	void Start () {
+		playerShipsOBJ = GameObject.FindGameObjectsWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
