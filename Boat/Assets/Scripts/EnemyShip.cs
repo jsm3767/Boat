1376fe6720df4460c30dd82a7a883deb3ba2ba25
Aria Myers (RIT Student) <jsm3767@ad.rit.edu>
2018-03-09 11:04:58 -0500

    Enemy Ai except Shooting
    Enemy Ai is all in except the shooting mechanics.  Might need tweaking with gameplay tests.

--- Boat/Assets/Scripts/EnemyShip.cs
+++ Boat/Assets/Scripts/EnemyShip.cs
@@ -26,15 +26,58 @@ public class EnemyShip : Ship {
 			shootTurn = true;
 		}
 
-		// yes - fire
+        // yes - fire
 
-		// Change direction to front of player and figure out what side to fire on
-			// only turn if angle to player is more than turn amount.
-			// ie angle < 45 no turn
-				// angle > 45 but < 90 turn 45
-	}
+        // Change direction to front of player and figure out what side to fire on
+        // only turn if angle to player is more than turn amount.
+        // ie angle < 45 no turn
+        // angle > 45 but < 90 turn 45
 
-	private GameObject findClosestPlayer(){
+        // vector to player
+        Vector3 toTarget = closestPlayer.transform.position - this.transform.position;
+        // get angle diff between lines
+        float angle = Mathf.Acos(Vector3.Dot(this.transform.forward, toTarget) / toTarget.magnitude);
+        //Debug.Log(Mathf.Rad2Deg*angle);
+        //now dtermine what side it's on.
+        float angleToRight = Mathf.Acos(Vector3.Dot(this.transform.right, toTarget) / toTarget.magnitude);
+        Debug.Log(angleToRight * Mathf.Rad2Deg);
+        if (angleToRight*Mathf.Rad2Deg < 90)
+        {
+            Debug.Log("on the right side");
+            if (Mathf.Rad2Deg * angle < 90)
+            {
+                turnDirection = TurnDirection._45R;
+            }
+            else
+            {
+                turnDirection = TurnDirection._90R;
+            }
+        }
+        else
+        {
+            if(Mathf.Rad2Deg * angle < 90)
+            {
+                turnDirection = TurnDirection._45L;
+            }
+            else
+            {
+                turnDirection = TurnDirection._90L;
+            }
+        }
+        if (Mathf.Rad2Deg * angle < 45)
+        {
+            turnDirection = TurnDirection.None;
+            shipSpeed = ShipSpeed.FullMast;
+        }else if(Mathf.Rad2Deg *angle > 90){
+            shipSpeed = ShipSpeed.None;
+        }
+        else
+        {
+            shipSpeed = ShipSpeed.HalfMast;
+        }
+    }
+
+    private GameObject findClosestPlayer(){
 		float dist = float.MaxValue;
 		GameObject closest = null;
 		foreach (GameObject g in playerShipsOBJ) {
--- Boat/Assets/Scripts/GameManager.cs
+++ Boat/Assets/Scripts/GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : Singleton<GameManager>
     //private GameObject[] playerShipsIcons;
 
     private List<GameObject> playerShips;
+    private List<GameObject> enemyShips;
 
     private float cameraMaxZoom;
     private float cameraMinZoom;
@@ -62,6 +63,7 @@ public class GameManager : Singleton<GameManager>
     void Start()
     {
         playerShips = new List<GameObject>(GameObject.FindGameObjectsWithTag("Player"));
+        enemyShips = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
         shipButtons = new List<GameObject>(GameObject.FindGameObjectsWithTag("CanvasButtons"));
     }
 
@@ -74,6 +76,10 @@ public class GameManager : Singleton<GameManager>
     public void DoTurn()
     {
         foreach (GameObject ship in playerShips)
+        {
+            ship.SendMessage("PlayTurn");
+        }
+        foreach (GameObject ship in enemyShips)
         {
             ship.SendMessage("PlayTurn");
         }
